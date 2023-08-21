af26ac4ad4e0d2baddceb74a23b8eb644b8df8ce
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-20 22:51:49 -0300

    aula 1

---
+++ 1/aula.py
@@ -0,0 +1,42 @@
+'''
+-- O que é Engenharia de Software?
+
+Engenharia de software é uma disciplina de
+engenharia relacionada com todos os
+aspectos produção de software, desde a
+especificação até a manutenção.
+
+Engenharia de software pode ser definida pelo
+estabelecimento e uso de sólidos princípios da
+engenharia para que se possa obter
+economicamente um software que seja confiável
+e que funcione eficientemente.
+
+-- O que é um processo de Software?
+
+É um conjunto de atividades cuja meta é o
+desenvolvimento ou evolução de software.
+As atividades genéricas em todos os processos de
+software são:
+
+● Especificação – o que o sistema deve fazer e
+suas restrições de desenvolvimento.
+● Desenvolvimento – produção do sistema de
+software.
+● Validação – verificação de que o software é o
+que o cliente deseja.
+● Evolução – mudança do software em resposta
+às demandas de mudança.
+
+-- O que são os métodos de Engenharia de Software?
+
+Abordagens estruturadas para o desenvolvimento de
+software que incluem modelos de sistema, notações, regras,
+recomendações de projeto e guias de processo.
+
+● Cascata
+● Prototipação
+● Incremental
+● Espiral
+
+'''
