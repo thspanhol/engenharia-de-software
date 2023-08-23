6fae1d6029253f6a62f5d81c274908b864334812
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-22 23:37:39 -0300

    aula 3

---
+++ 3/aula.py
@@ -0,0 +1,245 @@
+'''
+-- Introdução aos métodos ágeis
+
+Os métodos ágeis apresentam uma nova visão sobre 
+o desenvolvimento de softwares, surgindo a partir da 
+década de 1990 como reflexo de uma mudança cultural 
+e de pensamento. Sua principal diferença em relação a 
+métodos tradicionais ou prescritivos é o enfoque nas 
+pessoas. Assim, o que está no centro é o próprio time, e 
+não os processos. Além disso, diferencia-se por possuir um 
+conjunto de valores, princípios e práticas próprios. Também 
+se caracteriza pela adaptação a novos fatores decorrentes 
+do desenvolvimento de projetos, alinhando-se às mudanças 
+exigidas pelo mercado e, por consequência, pelos clientes.
+
+- Diferenças entre métodos tradicionais e métodos ágeis
+
+As diferenças entre métodos tradicionais e métodos 
+ágeis podem ser avaliadas a partir de diferentes critérios: 
+os pressupostos fundamentais, o direcionamento do 
+controle, o estilo de gerenciamento, a atribuição de 
+papéis, as características do ciclo do projeto, a estrutura 
+organizacional...
+Sob essa perspectiva, pode-se apontar que os métodos 
+tradicionais possuem sistemas totalmente previsíveis, 
+desenvolvidos a partir de um planejamento inicial, extenso 
+e meticuloso. O controle se centra nos processos, com 
+um gerenciamento de controle e comando, a partir da 
+distribuição de papéis individuais e especializados. O 
+projeto opera por tarefas ou atividades, possuindo uma 
+estrutura organizacional burocrática.
+Os métodos ágeis, por sua vez, possuem sistemas 
+adaptativos e de alta qualidade, que podem ser 
+desenvolvidos com melhoria contínua, a partir da exigência 
+de mudanças que surgem ao longo dos processos. 
+Com controle focado nas pessoas, possui um estilo de 
+gerenciamento colaborativo, com times autogerenciáveis, 
+favorecendo a troca de papéis. O projeto se direciona para 
+as funcionalidades do produto, possuindo uma estrutura 
+organizacional orgânica.
+
+-- Princípios ágeis
+
+1. As entregas de software devem ser feitas cedo, com 
+frequência e agregando valor ao negócio do cliente. 
+Inserindo-se no ciclo de entrega que será validado pelo 
+cliente, é fundamental que as entregas de software em 
+funcionamento mantenham a qualidade.
+
+2. A adequação a mudanças de requisito feitas pelo cliente, 
+mesmo no fim do projeto de desenvolvimento. Utilizando 
+as técnicas e ferramentas corretas, todas as mudanças 
+que possam vir a ser necessárias ou exigidas pelo cliente 
+são resolvidas de forma mais rápida. Ao prestar o auxílio 
+contínuo ao cliente, atendendo-o sempre da melhor forma, 
+também se permite que ele tire vantagens competitivas no 
+mercado.
+
+3. O iterativo incremental determina que as entregas, 
+validações e produções devem ser constantes, frequentes 
+e com qualidade. Para cada um desses processos, é 
+importante estabelecer um prazo de duas semanas ou de 
+até dois meses, com preferência para os prazos mais curtos.
+
+Avançando nos princípios ágeis, tem-se aqueles 
+relacionados ao trabalho em conjunto e à motivação do 
+time:
+
+4. O trabalho entre desenvolvedores e pessoas do negócio
+deve ser feito em conjunto. Isso significa que todas as 
+pessoas envolvidas no projeto devem trabalhar unidas e 
+com foco em torno do mesmo objetivo. Dessa forma, se 
+garante um fluxo contínuo de apresentação, discussão e 
+feedback sobre o que está sendo desenvolvido.
+
+5. Os indivíduos que atuam no desenvolvimento dos 
+projetos devem estar sempre motivados. Para isso, é 
+importante que se construa um ambiente com motivação, 
+em que tenham acesso a todas as ferramentas e ao suporte 
+necessário para executar seus trabalhos.
+
+Dando seguimento aos princípios determinados no 
+Manifesto Ágil, são estabelecidos parâmetros sobre 
+a interação das equipes, medidas de progresso, 
+desenvolvimento sustentável e excelência técnica:
+
+6. O método mais eficiente para se transmitir informação 
+em uma equipe de desenvolvimento é estar no mesmo 
+ambiente, promovendo conversas face a face. Assim, 
+além de garantir a interação, se tem mais compreensão 
+da comunicação, diminuindo o surgimento de conflitos e 
+tornando o trabalho mais eficaz. O face a face pode ser 
+incluído no modelo on-line, com o uso das câmeras dos 
+computadores.
+
+7. A principal medida de progresso em metodologias ágeis 
+é o software em funcionamento, ou seja, o quanto já foi 
+entregue do software. Isso significa que o andamento do 
+projeto de desenvolvimento deve ser mensurado pelas 
+entregas já realizadas, e não pela documentação extensa. 
+Para que esse processo seja bem-sucedido, é fundamental 
+ter a pessoa do negócio envolvida na validação do 
+processo, além de seguir os ciclos definidos de time box.
+
+8. Os processos ágeis devem promover o desenvolvimento 
+sustentável, com a capacidade de manter ritmos de 
+produção constantes. Com a definição dos ciclos de 
+trabalho, em que se trabalha coletivamente com os clientes, 
+todos os processos são bem definidos e constantes. Assim, 
+se cria um ambiente de trabalho menos estressante e 
+bastante produtivo, sendo por isso mais sustentável.
+
+9. A atenção à excelência técnica aumenta a agilidade. 
+Ou seja, a constante busca e atualização por aquilo que é 
+mais novo no mercado contribui com o desenvolvimento de 
+bons códigos que, junto a projetos de qualidade, tornam a 
+execução mais ágil.
+
+10. A simplicidade das execuções é delimitada de acordo 
+com o que é melhor para o negócio. Para que se saiba 
+priorizar o que deve ser feito no projeto, é fundamental a 
+figura do Product Owner (PO), que auxilia a entender o 
+que realmente precisa ser desenvolvido, de acordo com as 
+demandas reais.
+
+11. As equipes são autogerenciadas e devem trabalhar 
+em conjunto, com foco no cliente e na entrega. Sem 
+haver comando de controle, são as equipes que se auto-organizam em todas as etapas: de identificação das tarefas, 
+desenvolvimento, envio para o cliente, homologação... Ao 
+atuarem em conjunto, com união e conhecimento detalhado 
+do escopo combinado com o cliente, o trabalho executado 
+atinge altos níveis de excelência.
+
+12. Em intervalo de tempos regulares, a equipe deve 
+realizar a retrospectiva. Esse momento tem por objetivo 
+refletir sobre como tornar a equipe mais efetiva, afinando e 
+ajustando seu comportamento de acordo com as avaliações 
+e necessidades. É geralmente trabalhada em torno de três 
+questões: pontos positivos, pontos a melhorar, e planos de 
+ação. Inserindo-se dentro de ciclos regulares de melhoria 
+contínua (PDCA), é possível aprender com o que não 
+funcionou e potencializar o que está operando bem. Assim, 
+o time cria performance e segue se aperfeiçoando, o que 
+incide na melhoria de suas entregas.
+
+- Conceitos básicos do Kanban
+
+A prática ágil Kanban consiste em um sistema visual de 
+controle e gestão de tarefas. Em um grande quadro são 
+utilizados cartões (como post-its, ou outros indicativos 
+com função similar) para sinalizar o andamento da 
+execução das tarefas pré-definidas. Utilizando os campos 
+de “do” (a fazer), “doing” (em andamento) e “done” (já 
+feito), são inseridos cartões para apontar como está o 
+desenvolvimento das tarefas do time. Conforme o processo 
+tem seguimento, os cartões são movidos entre os campos, 
+mostrando a progressão das demandas. 
+Com surgimento relacionado ao sistema Toyota e ao 
+contexto industrial, o Kanban foi inicialmente utilizado em 
+projetos ágeis como ferramenta ou prática, tornando-se um 
+método em meados de 2007.
+
+- Conceitos básicos do XP
+
+Também chamado de “programação extrema”, o XP se 
+caracteriza pela produção de software de qualidade 
+em equipes pequenas e médias. Com adaptabilidade 
+às mudanças, esse método tem como foco a melhor 
+produção de software, se moldando às evoluções que sejam 
+necessárias. Para isso, utiliza uma abordagem de trabalho 
+colaborativa, com times que trabalham em conjunto, em um 
+ambiente livre de desconfiança e com a adoção de boas 
+práticas de engenharia de software. 
+Uma prática muito conhecida do XP é a chamada 
+“programação em par”. Nesse modelo, os profissionais 
+trabalham em dupla, compartilhando o mesmo 
+equipamento, sendo uma pessoa responsável pelo 
+desenvolvimento e outra pela análise, verificação e revisão 
+do código, em processos que ocorrem simultaneamente.
+
+- Conceitos básicos do TDD
+
+O TDD, sigla para “Test-Driven Development”, surgiu como 
+uma derivação do XP, propondo o teste do código antes 
+de seu desenvolvimento. Sendo um “desenvolvimento 
+dirigido a testes” ou “desenvolvimento baseado em testes”, 
+parte da escrita de um caso de teste automatizado, que 
+irá incluir as melhorias necessárias na funcionalidade, para 
+então se produzir o código que será validado pelo teste. É 
+uma prática bastante difícil, já que propõe uma mudança 
+significativa na forma de conceber os códigos. Mesmo 
+assim, possui muitos benefícios, já que a automatização de 
+testes resulta na entrega de códigos com mais qualidade.
+
+- Conceitos básicos do FDD
+
+O FDD, “Feature-driven development”, é o desenvolvimento 
+dirigido por funcionalidades, focando todo o processo de 
+desenvolvimento em torno dos requisitos funcionais do 
+sistema. Por essa prática, as funcionalidades são planejadas, 
+visualizadas, detalhadas e construídas em concepções 
+e planejamentos próprios, utilizando boas práticas de 
+engenharia de software. Seu objetivo central está na 
+entrega de softwares funcionais com agilidade.
+
+- Conceitos básicos do Scrum
+
+Criado na década 1990, o Scrum é um framework utilizado 
+para gerenciar o desenvolvimento de produtos complexos. 
+Não se trata de um processo, nem de uma técnica, mas 
+sim de um modelo de trabalho para desenvolvimento de 
+produtos, em que outros processos ou técnicas podem 
+ser mobilizados. Como documento de orientação do 
+framework, tem-se o “Guia do Scrum” (Scrum Guides), 
+atualizado a cada dois anos.
+
+O Scrum prevê uma série de “cerimônias”. Em primeiro 
+lugar, parte-se do product backlog, que lista todos os 
+requisitos que devem constar no produto – ou seja, tudo 
+que é preciso desenvolver. Depois, parte-se para a sprint 
+planning, uma espécie de reunião de planejamento, em que 
+se alinham as tarefas que serão desenvolvidas. Em seguida, 
+chega-se à sprint backlog, com a listagem de tudo que 
+precisará ser desenvolvido pelo time. Avança-se então para 
+a sprint, em que o time de Scrum atuará na execução das 
+demandas. Na daily meeting, o time realiza rápidas reuniões 
+diárias para relatar quais atividades estão sendo feitas e 
+para compartilhar eventuais dificuldades. A sprint review
+acontece no final do ciclo da sprint, com a presença de 
+todas as partes envolvidas no projeto, sendo um momento 
+para apresentar e verificar o resultado das atividades 
+desenvolvidas no período. Por fim, na retrospectiva são 
+avaliados todos os saldos positivos, os aspectos que devem 
+ser melhorados e os planos de ação para o próximo ciclo.
+ 
+É importante também compreender que o Scrum se baseia 
+em três pilares: transparência, inspeção e adaptação. Na 
+transparência, define-se que todos os aspectos do Scrum 
+devem ser visíveis e conhecidos por todos os participantes. 
+A inspeção indica que os usuários devem frequentemente 
+inspecionar os artefatos e progressos do Scrum que estão 
+sendo desenvolvidos. A adaptação, por sua vez, se refere 
+aos ajustes de tudo que foi verificado na etapa de inspeção, 
+buscando minimizar desvios. 
+'''
