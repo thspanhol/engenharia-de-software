63f3a15be0fc8a4209465ef16bcd7cab50bfb77b
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-23 22:09:54 -0300

    aula 4

---
+++ 4/aula.py
@@ -0,0 +1,67 @@
+'''
+-- O que é o Scrum?
+
+Em 1986, Takeuchi e Nonaka publicaram um estudo na
+Harvard Business Review no qual comparavam equipes
+de alto desempenho e multidisciplinares com a formação
+Scrum, oriunda das equipes de rugby. O propósito dessa
+formação é planejar estratégias antes de seguir com novas
+jogadas. Anos depois, em 1995, o Scrum foi formalizado por
+Jeff Sutherland e Ken Schwaber.
+Scrum não é um processo, uma técnica ou um método
+definitivo. Trata-se de um framework no qual podemos
+empregar diversos processos e técnicas.
+
+- Valores do Scrum
+
+a) Compromisso: o time Scrum compromete-se a atingir
+seus objetivos e suportar uns aos outros;
+b) Foco: o principal foco do Scrum é o trabalho da Sprint,
+visando conquistar o melhor progresso possível na direção
+dessas metas;
+c) Abertura: o time Scrum e seus stakeholders são abertos
+quanto ao trabalho e os desafios;
+d) Respeito: os membros do time Scrum se respeitam
+quanto a serem pessoas capazes e independentes, e são
+respeitados como tal pelas pessoas com quem trabalham;
+e) Coragem: os membros do time Scrum têm a coragem de
+fazer a coisa certa e de trabalhar em problemas difíceis.
+
+-- Kanban
+
+O termo “Kanban”, de origem japonesa, significa
+“sinalização” ou “cartão”. Sua aplicação no universo dos
+negócios sugere o uso de cartões (post-its) para indicar
+e acompanhar o andamento da produção no contexto da
+indústria. O Kanban é, portanto, um sistema ágil e visual
+para controle e gestão de tarefas, o qual busca gerenciar o
+trabalho conforme ele se movimenta pelo processo.
+O surgimento do Kanban remonta ao final dos anos 1940,
+quando a Toyota introduziu o modelo de fabricação “just
+in time” em sua produção de automóveis. Esse modelo é
+baseado na demanda do cliente, ao invés da prática então
+vigente de produzir certas quantidades de mercadorias e
+empurrá-las ao mercado. Foi do chamado “Sistema Toyota
+de Produção” que o Kanban se originou.
+O propósito central do Kanban é minimizar as atividades,
+sem sacrificar a produtividade; é criar mais valor para o
+cliente, sem gerar mais custos.
+
+- Práticas do Kanban
+
+1) Visualização do fluxo de trabalho usando a tabela/quadro
+e o cartão Kanban, de forma que as equipes tenham o
+conhecimento pleno desse fluxo;
+2) Limitação do trabalho em andamento, ou o “work in
+progress” (WIP), identificando o valor máximo de atividades
+em cada coluna do quadro Kanban;
+3) Gerenciamento do fluxo, tornando-o rápido e tranquilo;
+4) Explicitação de políticas, de formas que os processos
+sejam definidos, publicados e socializados claramente;
+5) Implementação de loops de feedbacks, analisando o fluxo
+de trabalho e promovendo mudanças, sempre visando a
+menor resistência possível;
+6) Melhorias na colaboração e evolução experimental,
+tornando colaborativos o processo e a proposição de
+soluções.
+'''
