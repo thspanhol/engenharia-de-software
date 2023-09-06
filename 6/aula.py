39158b3f0aa120224f742ed69899c95e046f05c9
Thales Spanhol <thales_spanhol@hotmail.com>
2023-09-05 23:10:35 -0300

    aula 6

---
+++ 6/aula.py
@@ -0,0 +1,124 @@
+'''
+ -- O que é Design Thinking? 
+Trata-se de uma abordagem centrada nas pessoas para
+concepção de soluções inovadoras por meio de processos
+criativos e analíticos.
+Algumas das ferramentas que compõem o Design Thinking,
+são: mapa da empatia, lean startup, seus 4 elementos chave
+e o Funil do conhecimento.
+O Design Thinking é definido em essência por três pilares:
+O que é Viável - O que é Factível - O que é Desejável
+
+- Valores do Design Thinking
+Empatia - se colocar no lugar do outro para entender
+melhor seus sentimentos, seu comportamento e seus
+desejos.
+Colaboração - trabalho feito em com uma ou mais pessoas;
+cooperação, ajuda, auxílio. Permitir que muitas pessoas
+possam trabalhar simultaneamente em um mesmo projeto.
+Experimentação - ênfase na ação, pela criação de
+experimentos e protótipos que serão testados pelos
+usuários e irão revelar soluções inovadoras.
+
+- Diamante duplo 
+Trata-se de um processo relacionado ao Design Thinking,
+que pode ser aplicado nos projetos para solucionar
+problemas e melhorar soluções existentes. É dividido em
+dois momentos chamados diamantes, e cada um deles tem
+etapas conhecidas como Divergentes e Convergentes.
+Framework do Design Thinking
+Etapas: descobrir, definir, desenvolver e entregar.
+Ações: empatizar, definir, idear, prototipar e testar.
+
+- Modos do Design Thinking
+Empatizar: desenvolver uma compreensão profunda do
+desafio.
+Definir: articular claramente o problema que você deseja
+resolver.
+Idear: sugerir, selecionar e desenvolver soluções potenciais.
+Prototipar: projetar protótipo(s) para testar, toda ou parte
+de sua solução.
+Testar: apresentar os protótipos aos usuários para obter
+feedback, refinar as soluções e aprender mais sobre eles.
+
+- Personas
+São representações de um usuário e seus objetivos,
+descrevendo não só o seu papel, mas também suas
+necessidades específicas.
+Persona é um personagem fictício construído a partir
+de etnografia de pessoas reais, com características
+psicológicas, que representa um grupo com qualidades e
+comportamentos parecidos.
+
+- Canvas da Empatia 
+O Canvas de Empatia é uma ferramenta visual para análise de
+um determinado público, ele permite compreender melhor os
+comportamentos de um cliente ou consumidor, ajudando a
+entender suas dores, necessidades e desejos.
+O Canvas do Mapa da Empatia é composto por 6 características
+a serem atribuídas a uma persona. Veja quais são essas
+características clicando nos números indicados.
+
+- Protótipos
+Protótipos são desenhos de aplicativos ou sistemas, ou um
+primeiro modelo que está em fase de teste. A proposta é
+criar uma representação semi realística de algo para que
+seja possível interagir e testar com usuários reais. Sua
+principal função é identificar problemas e oportunidades
+de melhorias muito antes de começar os refinamentos e a
+implementação.
+Dimensões de um protótipo, definidas pela autora Deborah
+J. Mayhew, no livro “The Usability Engineering Lifecycle”.
+1 - Detalhamento
+2 - Grau de funcionalidade
+3 - Similaridade de interação
+4 - Refinamento estético
+
+- Tipos de Protótipos 
+Protótipos de Baixa Fidelidade é utilizado em fases iniciais
+e exploratórias de um projeto para validar um conceito e
+decidir se uma ideia tem ou não valor funcional.
+Protótipos de Média Fidelidade é um pouco mais refinado
+que o de baixa fidelidade, mas ainda não necessita de
+uma preocupação estética. Conhecido como wireframe,
+é utilizado quando o foco é validar a arquitetura da
+informação e a interatividade com os elementos da
+interface.
+Protótipos de Alta Fidelidade deve se aproximar ao
+máximo dos aspectos visuais e funcionais do produto final,
+incluindo o conteúdo, fluxo de navegação e interações.
+Muito utilizados para testes e validação com usuários, ou
+para vender uma ideia.
+
+- IHC – Interface Humano Computador
+A Interface Humano Computador é uma área dentro da
+informática que objetiva fornecer aos pesquisadores e
+desenvolvedores de sistemas, explicações e previsões
+para fenômenos de interação usuário-sistema e resultados
+práticos para o design da interface de usuário.
+
+- Heurísticas
+Se relaciona à arte de inventar, fazer descobertas de fatos,
+busca por respostas adequadas para perguntas difíceis.
+
+- Heurísticas de Nielsen
+As Heurísticas de Nielsen foram criadas em 1994 pelo
+cientista da computação Jakob Nielsen, a partir de anos
+de estudos sobre usabilidade. São diretrizes de design
+que orientam e podem ser aplicadas na criação da
+maioria das interfaces de produtos e serviços digitais ou
+físicos, com o intuito de promover boas experiências de
+uso.
+
+1 - Visibilidade do estado do sistema.
+2 - Correspondência entre o sistema e o mundo real.
+3 - Controle e liberdade do usuário.
+4 - Consistência e padronização.
+5 - Prevenção de erros.
+6 - Reconhecimento em vez de lembrança.
+7 - Flexibilidade e eficiência de uso.
+8 - Projeto estético e minimalista.
+9 - Auxilia os usuários a reconhecerem, diagnosticarem e se
+recuperarem de erros.
+10 - Ajuda e documentação.
+'''
