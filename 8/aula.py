1d895046d5fb24cd9f48d9985a14d41f3e6958b1
Thales Spanhol <thales_spanhol@hotmail.com>
2023-09-07 22:10:05 -0300

    aula 8

---
+++ 8/aula.py
@@ -0,0 +1,79 @@
+'''
+-- Implementação de Software
+Com a definição dos componentes, classes ou serviços 
+do projeto detalhado, há a implementação do software. 
+Ana Paula afirma que se tem como meta, dentro desta 
+implementação, a construção de uma versão de software 
+executável para que o cliente tenha o recurso desenvolvido 
+e possa, assim, executar a sua aplicação.
+Para o desenvolvimento da versão executável, é necessário: 
+linguagens de programação, customização de softwares 
+de prateleira ou algum tipo de integração de outros 
+componentes de software.
+
+- Áreas de Atuação
+A docente traz as áreas profissionais de atuação de um 
+desenvolvedor/implementador de software:
+• Desenvolvimento Back-end: desenvolvimento de regras 
+de negócios dentro da estrutura interna do software.
+• Desenvolvimento Front-end: aplicação e integração da 
+solução do ponto de vista do cliente.
+• Desenvolvimento Full-stack: profissional completo (tanto 
+back-end quanto front-end).
+• Desenvolvimento desktop: solução específica para um 
+computador.
+• Desenvolvimento WEB: solução específica para a Web.
+• Desenvolvimento Mobile: solução para os smartphones.
+• Desenvolvimento de Jogos.
+
+- Hard e Soft Skills dos Desenvolvedores de Software
+
+-Hard Skills
+• Desenvolvimento Back-end: PHP, Java, Ruby, Python.
+• Desenvolvimento Front-end: HTML (linguagem de 
+marcação), CSS (linguagem de estilos), JavaScript.
+• Desenvolvimento Full-stack: linguagens básicas, 
+mas se destaca por entender toda a estrutura de 
+desenvolvimento e conseguir entregar um projeto 
+completo.
+• Desenvolvimento desktop: Java, C# e Delphi.
+• Desenvolvimento WEB: front-end e Back-end, que são 
+as especialidades que você pode adotar na sua carreira. 
+Se não, vale seguir as orientações do desenvolvedor 
+Full-stack.
+• Desenvolvimento Mobile: Kotlin, que vem substituindo 
+o Java nas aplicações para Android. Swift, que vem 
+substituindo o Objective-C no iOS.
+• Desenvolvimento de Jogos: C++ e o C#.
+
+-Soft Skills
+• Capacidade de resolução de problemas;
+• Atenção aos detalhes;
+• Comunicação;
+• Espírito de equipe;
+• Pensamento crítico;
+• Automotivação.
+
+- Teste de Software
+A partir de um software implementado faz-se necessário 
+executar a validação da solução dentro do contexto de 
+evolução do software. A realização do teste trará como 
+resultados a aplicabilidade e a execução do software e, 
+também, as devidas correções que deverão ser realizadas. 
+
+- Conceito de Teste
+• Verificação: avalia se o software atende aos requisitos 
+definidos. 
+• Validação: avalia se o software atende às expectativas do 
+cliente.
+• Erro: é o resultado de um engano humano. 
+• Defeito (bug): é a inclusão do erro no software.
+• Falha: é a execução anormal do software devido aos 
+defeitos.
+• Teste: atividade de avaliar a correção do software em 
+busca de falhas. 
+• Depuração (debugging): atividade de localizar o defeito 
+no software.
+• Manutenção corretiva: atividade de remover o defeito do 
+software. 
+'''
